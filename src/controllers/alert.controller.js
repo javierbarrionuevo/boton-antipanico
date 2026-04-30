93d1d53681fc9be8b87f4b7612fa1067a65ab08a
Javier Barrionuevo <jnuevoo@gmail.com>
2026-04-30 16:38:49 -0300

    fix

--- src/controllers/alert.controller.js
+++ src/controllers/alert.controller.js
@@ -4,13 +4,18 @@ const { sendAlert } = require('../services/telegram.service');
 exports.createAlert = (req, res) => {
   const { device_id } = req.body;
 
+  console.log("Alerta de:", device_id);
+
   const chatId = getChatIdByDevice(device_id);
 
   if (!chatId) {
+    console.log("❌ No hay usuario vinculado");
     return res.status(404).json({ error: "Dispositivo no vinculado" });
   }
 
-  sendAlert(chatId, `🚨 ALERTA de ${device_id}`);
+  console.log("📲 Enviando a:", chatId);
+
+  sendAlert(chatId, `🚨 ALERTA del dispositivo ${device_id}`);
 
   res.json({ ok: true });
 };
